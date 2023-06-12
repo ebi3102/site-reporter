e6e15382c37d0b5e422dc37f79b3aad9e6af1ea2
ebi3102 <ebi3102@gmail.com>
2023-06-12 09:59:34 +0330

    Fix the bug of async

--- index.js
+++ index.js
@@ -20,24 +20,29 @@ let apiKey = '619e73623914fb75094ff874700118b7cb7cbf229eb6bfb1281d0a48';
 json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
   dataIP = data.ip;
   const date = new Date();
-  let stringDate = date.getFullYear() + '-' + date.getMonth()+'-'+date.getDay()+'_h'+date.getHours();
+  let stringDate = date.getFullYear() + '-' + (date.getMonth()+1)+'-'+date.getDate()+'_h'+date.getHours();
   let directory_name = './screenshots/'+ stringDate +'_'+ dataIP
-  // let directory_name = './screenshots/';
   if(!fs.existsSync(directory_name)){
     fs.mkdirSync(directory_name, { recursive: true })
   }
 
   line_reader('urls.txt')
   .then(lines => {
-      // Store the lines array in a variable
-      const urls = lines
-      urls.map(url=>{
-          if(url != ''){
-              viewportSizes.map(currentWidth=>{
-                  captureScreenshot(url, directory_name, currentWidth, 3000);
-              })
-          }  
-      })  
+    // Store the lines array in a variable
+    const urls = lines;
+    (async function(){
+      for await(const url of urls){
+        if(url != ''){
+          console.log(url);
+          await (async function(){
+            for await(const currentWidth of viewportSizes){
+              // console.log('--',url,': ',currentWidth);
+              await captureScreenshot(url, directory_name, currentWidth, 3000);
+            }
+          })();                
+        } 
+      }
+    })();  
   })
   .catch(error => {
       console.error('Error:', error);
