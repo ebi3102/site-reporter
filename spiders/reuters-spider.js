72d875353b815697dd3c524c87b0b3243c9570c9
ebi3102 <ebi3102@gmail.com>
2024-01-01 16:26:06 +0330

    Init read_page_content function

--- spiders/reuters-spider.js
+++ spiders/reuters-spider.js
@@ -8,28 +8,27 @@ async function reutersSpider(url, directory_name, targetDomain){
     const page = await browser.newPage();
 
     await page.goto(url);
-
+    await page.waitForSelector('li.story-collection__story__LeZ29 div.media-story-card__body__3tRWy a');
     const links = await page.evaluate(() => {
-      const anchorElements = document.querySelectorAll('a');
-    // const anchorElements = document.querySelectorAll('li.story-collection__story__LeZ29 div.media-story-card__body__3tRWy>a');
+    const anchorElements = document.querySelectorAll('li.story-collection__story__LeZ29 div.media-story-card__body__3tRWy a');
     let Elem = [];
-    for(var i=0; i<anchorElements.length; i++){Elem.push(anchorElements[i].href)}
+    for(var i=0; i<anchorElements.length; i++){
+      Elem.push(anchorElements[i].href);
+      console.log("links", anchorElements[i].href);
+    }
     return Elem;
     });
 
-
     let uniqueLinks = [...new Set(links)];
-    console.log(links);
     // var targetDomain = "pcmfa.co"; //TODO: set as config
     const filteredUrls = uniqueLinks.filter(url => {
       if(url){
         const parsedUrl = new URL(url);
-        return parsedUrl.hostname.endsWith(targetDomain);
+        return parsedUrl.hostname.match(targetDomain)
+        // return parsedUrl.hostname.endsWith(targetDomain);
       }
     });
-
-    // const filteredUrls = uniqueLinks;
-
+    console.log(filteredUrls);
     for (const link of filteredUrls) {
       await write_log(
         `${directory_name}/hack.log`,
@@ -49,6 +48,61 @@ async function reutersSpider(url, directory_name, targetDomain){
 };
 
 module.exports = reutersSpider;
+
+async function read_page_content(url)
+{
+  try{
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+
+    await page.goto(url);
+    // await page.waitForSelector('li.story-collection__story__LeZ29 div.media-story-card__body__3tRWy a');
+    const pageContent = await page.evaluate(() => {
+      let content = {};
+      content['headTitle'] = document.title;
+
+      /**
+       * finde meta in head and save validMetaTags
+       */
+      const metaTags = document.head.querySelectorAll('meta');
+      const validMetaTags = ['description']
+      let metaDescription = null;
+      for (const metaTag of metaTags) {
+        if (validMetaTags.includes(metaTag.name)) {
+          metaDescription = metaTag.content;
+          content[metaTag.name] = metaDescription;
+        }
+      }
+
+      /**
+       * find application/ld+json scripts
+       */
+      const scriptTags = document.querySelectorAll('script');
+      const ldJsonScripts = [];
+      scriptTags.forEach(script => {
+        if (script.type === 'application/ld+json') {
+          // Parse the JSON content and add it to the ldJsonScripts array
+          try {
+            const ldJsonContent = JSON.parse(script.textContent);
+            ldJsonScripts.push(ldJsonContent);
+          } catch (error) {
+            console.error('Error parsing JSON in application/ld+json script:', error);
+          }
+        }
+      });
+      content['ldJsons'] = ldJsonScripts;
+
+      const contentHeader = document.querySelector('div.default-article-header__container__3NcV7');
+
+
+
+    
+    });
+
+  }catch{
+
+  }
+}
 
 async function linke_checker(patterns, link, logPath, pageUrl){
   try{
