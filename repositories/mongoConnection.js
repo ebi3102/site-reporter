106325eaef4359e3e1504171e3bed966d016de1b
ebi3102 <ebi3102@gmail.com>
2024-01-04 16:06:07 +0330

    complete mongo connection

--- repositories/mongoConnection.js
+++ repositories/mongoConnection.js
@@ -3,13 +3,13 @@ const { mongodbUri, dbName } = require('../app.config');
 
 const connectToMongo = async (collectionName) => {
   try {
-    const client = await MongoClient.connect(mongodbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+    const client = new MongoClient(mongodbUri);
     const db = client.db(dbName);
     const collection = db.collection(collectionName);
 
     return { client, collection };
   } catch (error) {
-    throw new Error('Error connecting to MongoDB: ' + error.message);
+    throw new Error('Error connecting to MongoDB: ' + error);
   }
 };
 
@@ -18,7 +18,7 @@ const insertData = async (collection, data) => {
     const result = await collection.insertOne(data);
     return result.ops;
   } catch (error) {
-    throw new Error('Error inserting data: ' + error.message);
+    throw new Error('Error inserting data: ' + error);
   }
 };
 
