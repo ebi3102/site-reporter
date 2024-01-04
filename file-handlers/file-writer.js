9c8bdb7588ff0c97baa2e16821375fde90457044
ebi3102 <ebi3102@gmail.com>
2024-01-04 15:17:58 +0330

    Add date and time to writer module

--- file-handlers/file-writer.js
+++ file-handlers/file-writer.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 async function write_log(filePath, text){
-    fs.appendFile(filePath, text,
+    fs.appendFile(filePath, `[${new Date()}] ${text}`,
       "utf8",
       function(err) {
         if(err) {
